9494f3937df94dd443ec1b9072431c3d0c3767be
zerofairnight <zerofairnight@gmail.com>
2019-02-22 16:51:11 +0100

    add parse_mode constants

---
+++ src/ParseMode.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Telegram;
+
+class ParseMode
+{
+    /**
+     * The Markdown parse_mode, case insensitive.
+     *
+     * @var string
+     */
+    const MARKDOWN = 'Markdown';
+
+    /**
+     * The HTML parse_mode, case insensitive.
+     *
+     * @var string
+     */
+    const HTML = 'HTML';
+}
